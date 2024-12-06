618917470c3424c77143361c92a2e2167f25c8fa
naonao2323 <yanagidanaoki9900@gmail.com>
2024-12-06 21:31:14 +0900

    experiment

--- src/layout/content.tsx
+++ src/layout/content.tsx
@@ -3,9 +3,14 @@ interface ContentProps {
 }
 
 export default function Content({props}: {props: ContentProps}) {
+    const handleVibrate = () => {
+        if (navigator.vibrate) {
+          navigator.vibrate(200);
+        }
+      };
     return (
     <div className={`min-h-screen min-w-full z-${props.zIndex} grid grid-cols-1 gap-4 md:grid-cols-4 2xl:grid-cols-6 p-4 w-1/4 mx-auto`}>
-    <div className="border-opacity-5 border-solid border-stone-100 h-48 flex items-center justify-center p-10 bg-white rounded-md">
+    <div className="border-opacity-5 border-solid border-stone-100 h-48 flex items-center justify-center p-10 bg-white rounded-md" onTouchStart={handleVibrate} >
         <h1 className="font-bold rounded-sm">Golang text/templateについて</h1>
     </div>
     </div>
