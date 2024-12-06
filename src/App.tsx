f330e271fe18c182ae0b247ea93d58cb01082d84
naonao2323 <yanagidanaoki9900@gmail.com>
2024-12-06 12:06:08 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="absolute inset-0 bg-white opacity-10 rounded-full blur-2xl w-2/3 h-1/3 top-16 left-1/3"></div>
     <div className="absolute inset-0 bg-orange-200 opacity-20 rounded-full blur-3xl w-1/2 h-1/4 bottom-20 right-1/4"></div>
     <div className="relative text-white flex items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold text-amber-200">ブログサンプル</h1>
+      <h1 className="text-4xl font-bold text-amber-200">ブログサンプルです......</h1>
     </div>
 </div>
 
